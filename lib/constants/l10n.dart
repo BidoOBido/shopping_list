2f7cbe551186125c19c6cad793be1a1e44b318b0
Mailon Bido <mailonbido@gmail.com>
2021-03-21 20:35:58 -0300

    Change l10n delegate list
    Add constant to keep l10n delegates list

---
+++ lib/constants/l10n.dart
@@ -0,0 +1,10 @@
+import 'package:shopping_list/components/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class L10n {
+  static const delegates = [
+    AppLocalizations.delegate,
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:shopping_list/components/app_localizations.dart';
 import 'package:shopping_list/constants/i18n.dart';
+import 'package:shopping_list/constants/l10n.dart';
 import 'package:shopping_list/constants/routes.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping_list/provider/items.dart';
@@ -28,11 +28,7 @@ class ShoppingList extends StatelessWidget {
       initialRoute: Routes.initialRoute,
       routes: Routes.routesMap,
       supportedLocales: I18N.suportedLocales,
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
+      localizationsDelegates: L10n.delegates,
       localeResolutionCallback: (locale, supportedLocales) {
         for (var supportedLocale in supportedLocales) {
           if (supportedLocale.languageCode == locale?.languageCode &&
